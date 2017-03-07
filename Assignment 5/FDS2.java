4568d5cecce7accd1fe1cd7d18524a5aaaff56e8
Garrett <gmiller1394@gmail.com>
2017-03-07 10:28:08 -0500

    Finished HW

--- Assignment 5/FDS2.java
+++ Assignment 5/FDS2.java
@@ -109,7 +109,11 @@ public class FDS2{
   }
 
  FD BCNFviolation(){
-	for (FD fd: F) if (!closure(fd.lhs).containsAll(R)) return fd;
+	for (FD fd: F) {
+    if (!closure(fd.lhs).containsAll(R)) {
+      return fd;
+    }
+  }
 	return null;
  }
 
@@ -134,10 +138,16 @@ public class FDS2{
 		}
 	  }
 	  System.out.println();
-	}else{  
+	}
+	else
+	{  
 		HashSet<Character> R1 = closure(fd.lhs);
 		HashSet<Character> R2 = new HashSet<Character>(fd.lhs);
-		for (char c: R) if (!R1.contains(c)) R2.add(c);  // decomposed into R1 and R2
+    	for (char c: R) {
+        if (!R1.contains(c)) {
+          R2.add(c);  // decomposed into R1 and R2
+        }
+      }
 		FDS2 fds1 = new FDS2(R1, project(R1, R1, new HashSet<Character>()));
 		fds1.BCNFdecompose();  // recursive call on R1
 		FDS2 fds2 = new FDS2(R2, project(R2, R2, new HashSet<Character>()));
@@ -152,24 +162,34 @@ public class FDS2{
 	// This recursive function generates all subsets of R1 in a depth-first way by set inclusion
 	HashSet<Character> Xplus = closure(X);  //  Step 2
 	if (X.size() > 0)
-	  for (char A: Xplus) if (A is not in X and A is in R1) T.add(new FD(X, A));
+      for (char A: Xplus) {
+        if ((!X.contains(A)) && R1.contains(A)) {
+          T.add(new FD(X, A));
+        }
+      }
 	if (!Xplus.containsAll(R)){  // otherwise no superset needs to be explored 
 // If we already know that the closure of X is all attributes, then we cannot discover any
 // new FD's by closing supersets of X. p.83
 		HashSet<Character> candidates2 = new HashSet<Character>(candidates);
 		for (char c: candidates){  // go through all superset of X by adding one attribute
-			candidates2.remove(A);
-			X.add(A);
+			candidates2.remove(c);
+			X.add(c);
 			T.addAll(project(R1, new HashSet<Character>(candidates2), 
 				new HashSet<Character>(X)));
-			X.remove(A);
+			X.remove(c);
 		}
 	}
 	return T;
   }
 
- public static void main(String[] args){
-    FDS2 fds = new FDS2(args[0]);     
-    fds.BCNFdecompose();
- }
+  public static void main(String[] args){
+    FDS2 fds_a = new FDS2("exercise331a.txt");
+    FDS2 fds_b = new FDS2("exercise331b.txt");
+
+    System.out.println("Exercise 3.31a: ");
+    fds_a.BCNFdecompose();
+
+    System.out.println("Exercise 3.31b: ");
+    fds_b.BCNFdecompose();
+  }
 }
